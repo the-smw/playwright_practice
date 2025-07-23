7e801dfa654428e2d7d09f4979f9937db29683ae
lia_barna <lia.barna@testifi.io>
2025-07-23 15:11:22 +0200

    [chore] update yml file

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 import {LoginPage} from '../pages/login-page';
 
-const url = process.env.URL!;
-const username = process.env.VALIDUSER!;
-const password = process.env.PASSWORD!;
+// const url = process.env.URL!;
+const url = 'https://training.testifi.io/';
+const username = process.env.CI_USER || process.env.VALIDUSER!;
+const password = process.env.CI_PW || process.env.PASSWORD!;
 let loginPage: LoginPage;
 
 test.use({ storageState: { cookies: [], origins: [] } }); // doesn't share the logged in session
