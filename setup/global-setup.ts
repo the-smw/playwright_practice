6ea73edae6ad68314213f58d4623309185486c84
lia_barna <lia.barna@testifi.io>
2025-07-23 15:17:24 +0200

    [fix] refactor global-setup

--- setup/global-setup.ts
+++ setup/global-setup.ts
@@ -2,15 +2,15 @@ import { chromium, FullConfig, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
 
 async function globalSetup(config: FullConfig) {
-    const user = process.env.VALIDUSER!;
-    const password = process.env.PASSWORD!;
-    const url = process.env.URL!;
+    const username = process.env.CI_USER || process.env.VALIDUSER!;
+    const password = process.env.CI_PW || process.env.PASSWORD!;
+    const url = 'https://training.testifi.io/';
     const storageStatePath = config.projects[0].use.storageState as string;
     const browser = await chromium.launch({ headless: true });
     const page = await browser.newPage();
     const loginPage = new LoginPage(page);
     await page.goto(url);
-    await loginPage.fillUsername(user);
+    await loginPage.fillUsername(username);
     await loginPage.fillPassword(password);
     await loginPage.clickLoginButton();
     await expect(loginPage.logoutButton).toBeVisible();
